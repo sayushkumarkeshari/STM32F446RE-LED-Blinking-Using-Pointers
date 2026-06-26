fa441d907b7db556ce8a0607467dd556cbd83fe4
sayushkumarkeshari <sayushkeshari@gmail.com>
2026-06-26 11:49:04 +0530

    Update main5.c
    Updated the STM32F446RE LED blinking program by using volatile pointers for hardware registers and bitwise operations for GPIO configuration and LED control. This improves the code by following standard embedded C programming practices for memory-mapped register access.

--- main5.c
+++ main5.c
@@ -1,28 +1,29 @@
 #include<stdio.h>
 
-	unsigned int *rcc_cr=(unsigned int *)0x40023800;
-	unsigned int *rcc_AHB1ENR=(unsigned int *)0x40023830;
-	unsigned int *GPIOA_MODER=(unsigned int *)0x40020000;
-	unsigned int *GPIOA_OTYPER=(unsigned int *)0x40020004;
-	unsigned int *GPIOA_SPEEDR=(unsigned int *)0x40020008;
-	unsigned int *GPIOA_ODR=(unsigned int *)0x40020014;
-	void delay(long delay)
+	volatile unsigned int *rcc_cr=(unsigned int *)0x40023800;
+	volatile unsigned int *rcc_AHB1ENR=(unsigned int *)0x40023830;
+	volatile unsigned int *GPIOA_MODER=(unsigned int *)0x40020000;
+	volatile unsigned int *GPIOA_OTYPER=(unsigned int *)0x40020004;
+	volatile unsigned int *GPIOA_SPEEDR=(unsigned int *)0x40020008;
+	volatile unsigned int *GPIOA_ODR=(unsigned int *)0x40020014;
+	void delay(volatile long delay)
 	{
 		while(delay--);
 	}
 	int main()
 	{
-		*rcc_cr=0x00000001;
-		*rcc_AHB1ENR=0x00000001;
-	  *GPIOA_MODER=0x00000400;
+		*rcc_cr |= (1<<0);// 0x00000001;
+		*rcc_AHB1ENR |=(1<<0); //0x00000001;
+		*GPIOA_MODER &= ~(3<<10);
+	  *GPIOA_MODER |=(1<<10); //0x00000400;
 	  *GPIOA_OTYPER=0x0000;
 	  *GPIOA_SPEEDR=0x00000000;
 		
 		while(1)
 		{
-	  *GPIOA_ODR=0x0020;
+	  *GPIOA_ODR |=(1<<5);
 			delay(500000);delay(500000);
-		*GPIOA_ODR=0x0000;
+		*GPIOA_ODR &=~(1<<5);
 			delay(500000);delay(500000);
 		}
 		
